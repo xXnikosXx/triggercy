9fb7c4b7597a1f81b699743cbbb0e846e8c562f9
Nikos <c.xxnikosxx@gmail.com>
2020-11-12 23:05:17 +0200

    Update targetRemove.js

--- commands/targetRemove.js
+++ commands/targetRemove.js
@@ -9,7 +9,7 @@ module.exports = {
 	// execute(message, args) {
 	execute(message) {
 		if (message.member.roles.cache.has(global.adminRole) == true) {
-			message.channel.send("Target pardoned! Have they been good? Do they deserve mercy? MASTER SHALL I FIRE UPON THEM? I CAN REIGN HELL UPON THEM JUST GIVE ME THE OKIE DOKIE AND I WILL JUST \n jus' you wait, JUS' YOU W \n *loads gun with holy, malicious intent* \n AWAITING YOUR COMMAND, 天帝!");
+			message.channel.send("Target pardoned!");
 			global.target = "000000000000000000";
 			global.targetName = global.targetName + "*PARDONED*";
 		}
